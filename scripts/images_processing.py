4f658fe0e31a170224d534b74551a651bea7ed62
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2022-03-23 22:10:26 +0100

    Add white paper detector to image_processing.py

--- scripts/images_processing.py
+++ scripts/images_processing.py
@@ -9,9 +9,38 @@ OUTPUT_IMG = './outputs/equalized.png'
 
 
 while True:
-    img = cv2.imread(LAST_IMG, cv2.IMREAD_GRAYSCALE)
-    img_eq = cv2.equalizeHist(img)
+    image = cv2.imread(LAST_IMG)
+    original = image.copy()
 
-    cv2.imwrite(OUTPUT_IMG, img_eq)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+
+    kernel = np.ones((3, 3))
+    image = cv2.erode(image, kernel, iterations=4)
+    image = cv2.dilate(image, kernel, iterations=4)
+
+    contours, _ = cv2.findContours(
+        image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    try:
+        contours = filter(lambda cont: cv2.arcLength(cont, False) > 1000, contours)
+        max_contour = max(contours, key=lambda cont: cv2.contourArea(cont))
+    except ValueError:
+        sleep(2)
+        continue
+
+    rect = cv2.minAreaRect(max_contour)
+    box = cv2.boxPoints(rect).astype(np.int0)
+    width, height = int(rect[1][0]), int(rect[1][1])
+
+    src_pts = box.astype("float32")
+    dst_pts = np.array([[0, height - 1],
+                        [0, 0],
+                        [width - 1, 0],
+                        [width - 1, height - 1]], dtype="float32")
+
+    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+    image = cv2.warpPerspective(original, M, (width, height))
+    cv2.imwrite(OUTPUT_IMG, image)
+
     sleep(1)
-
